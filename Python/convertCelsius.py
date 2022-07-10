ab3df996644281bfb101eb0eaed855aad47c5858
Mohamed Kharma <77759231+mohamedkharma@users.noreply.github.com>
2022-07-10 09:01:27 -0400

    Create convertCelsius.py

---
+++ Python/convertCelsius.py
@@ -0,0 +1,9 @@
+celsius_temp = int(input("Enter the Celsius temperature you would like to convert: "))
+fahrenheit = celsius_temp * 1.8 + 32
+kelvin = celsius_temp  + 273.15
+reaumur =  celsius_temp * 0.8
+rankine = celsius_temp * 1.8 + 32 + 459.67
+
+print (f"fahrenheit: {fahrenheit} \nkelvin: {kelvin} \n\
+reaumur: {reaumur} \nrankine:  {rankine}")
+            
